1f124342c64b24b11e0d7f560b760322b688cd0a
Lucas Feitoza Gomes <72843674+Lukasx76@users.noreply.github.com>
2023-05-09 10:30:55 -0300

    Update awsRestartChallenge.sh

--- awsRestartChallenge.sh
+++ awsRestartChallenge.sh
@@ -1,26 +1,17 @@
 #!/usr/bin/bash
 
-#Extracting the greatest number of the files considering that the current directory is /home/lucas
-var=`ls -1v lucasGomes* | tail -1 | tr -d -c 0-9`
+start=`ls -1v lucasGomes* | grep [0-9]$ | tail -1 | tr -d lucasGomes`
 
-#The number extracted is stored into a variable and it will be considered the starting number or the least number of the loop
-leastNumber=$var
-
-#I want to generate 25 files starting from leastNumber so the least number will be added 25 turning into the ending number or Greatest number of the loop
-greatestNumber=$var+25
-
-#also, I want to check that the current directory is /home/lucas 
-CURRENT_DIRECTORY=pwd
-DIRECTORY_TO_RUN='/home/lucas'
-
-if [ `$CURRENT_DIRECTORY`==`$DIRECTORY_TO_RUN` ]
+if [ -z $start ]
 then
-        i=$leastNumber
-        while [[ $i -le $greatestNumber ]]
-        do
-                touch lucasGomes$i
-                ((i = i + 1))
-        done
-else
-        echo "You need to be on /home/lucas directory to run this script"
+        start=1
 fi
+
+range=start+25
+
+for (( c=start; c<range; c++ ))
+do
+        touch "lucasGomes$c"
+done
+
+#adjusting the code
